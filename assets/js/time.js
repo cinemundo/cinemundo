6b91c3a3e1fc7a002648f2080ed917610a626fa2
Vinicius Marques <marquess.vini@gmail.com>
2023-02-11 03:34:53 -0300

    Atualizações de design e melhorias na performance dos autores do site em sinergia com o restante do layout.

--- assets/js/time.js
+++ assets/js/time.js
@@ -4,30 +4,40 @@ const equipe = [
     descricao:
       "Fanático por séries de ficção científica e apaixonado por tecnologia, a telinha da tv me encanta e me faz viajar por onde a imaginação levar.",
     imagem: "time_foto_roberto_zanin.png",
+    instagram: "https://www.instagram.com/autor1",
+    github: "https://github.com/raszanin"
   },
   {
     nome: "Rosângela Melanias",
     descricao:
       "Cinéfila desde os 12 anos e já via filmes em VHS. Adora filmes inteligentes, suspenses e terror. Para maratonar uma série, não dispensa um guaraná e pipoca!",
     imagem: "time_foto_rosangela_melanias.png",
+    instagram: "https://www.instagram.com/autor1",
+    github: "https://github.com/RoMelanias"
   },
   {
     nome: "Thiago Manfredi",
     descricao:
       "Geek que curte ficção científica, animação e fantasia. Também não dispensa eventualmente um bom drama ou romance. Acumulador no 'Minha Lista'.",
     imagem: "time_foto_thiago_manfredi.png",
+    instagram: "https://www.instagram.com/autor1",
+    github: "https://github.com/thiagoManfredi"
   },
   {
     nome: "Vinícius Marques",
     descricao:
       "Gosto de me sentir imerso nos filmes. Prefiro telas grandes e ficção científica, mas às vezes só me resta série teen e o celular, e eu amo!",
     imagem: "time_foto_vinicius_marques.png",
+    instagram: "https://www.instagram.com/sumyvinsil",
+    github: "https://github.com/ViniciusSumy"
   },
   {
     nome: "Viviane Favato",
     descricao:
       "Apaixonada por filmes clássicos e comédias, mas chegadinha num drama pra chorar. Adora maratonar séries nos finais de semana.",
     imagem: "time_foto_viviane_favato.png",
+    instagram: "https://www.instagram.com/autor1",
+    github: "https://github.com/Vibarbara"
   },
 ];
 
@@ -36,10 +46,22 @@ const exibirDescricao = (index) => {
   descricao.innerHTML = `
     <p class="nome-pessoa">${equipe[index].nome}</p>
     <em>${equipe[index].descricao}</em>
-    <a href="${equipe[index].instagram}" target="_blank"><i class="fa fa-instagram"></i></a>
-    <a href="${equipe[index].github}" target="_blank"><i class="fa fa-github"></i></a>
+    <div>
+    <a href="${equipe[index].instagram}" target="_blank">
+      <i class="fab fa-instagram fa-2x"></i>
+    </a>
+    <a href="${equipe[index].github}" target="_blank">
+      <i class="fab fa-github fa-2x"></i>
+    </a>
+      <i class="fas fa-star"></i>
+      <i class="fas fa-star"></i>
+      <i class="fas fa-star"></i>
+      <i class="fas fa-star"></i>
+      <i class="fas fa-star"></i>
+      </div>
   `;
 };
+
 
 const mosaicos = document.querySelectorAll(".mosaico");
 mosaicos.forEach((mosaico, index) => {
